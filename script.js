95ede541b91ffaad3a232772cea28012de278566
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-10-27 00:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+// Get references to the HTML elements
+const statusParagraph = document.getElementById("status");
+const enterButton = document.getElementById("enterBtn");
+
+// Add a click event listener to the button
+enterButton.addEventListener("click", function () {
+    // Change the content of the paragraph to "Entered Metaverse"
+    statusParagraph.textContent = "Entered Metaverse";
+});
